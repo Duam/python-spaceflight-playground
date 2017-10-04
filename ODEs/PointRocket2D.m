b43621a75fc00a4938335a303a4db9de7d8d519e
Paul <paul@kiteswarms.com>
2017-10-04 15:15:38 +0200

    Changed scaling to kilometers and microrads. Changed state 1 from distance to altitude

--- ODEs/PointRocket2D.m
+++ ODEs/PointRocket2D.m
@@ -1,15 +1,18 @@
-%% ---------------- Parameters (All in kg, m, s) --------------------------
+%% ---------------- Parameters (All in kg, km, s) --------------------------
 % -- Physical parameters --
-G = 6.67408 * 10^-11;   % Gravitational constant
-M = 7.348 * 10^22;      % Moon mass
-mu = G * M;             % Standard gravitational parameter
-R = 1.7375 * 10^6;      % Moon radius
+% G  = 6.67408 * 10^-11;          % Grav. const. (m^3 * kg^-1 * s^-2)
+G  = 6.67408 * 10^-20;          % Grav. const. (km^3 * kg^-1 * s^-2)
+M  = 7.348 * 10^22;             % Moon mass (kg)
+mu = G * M;                     % Std. grav. param. (m^3 * s^-2)
+% R  = 1737.5 * 10^6;             % Moon radius (m)
+R  = 1737.5 * 10^3;             % Moon radius (km)
 
 % -- Rocket parameters --
-m0   = 20 * 10^3;      % Initial mass of rocket
-m_e   =  1 * 10^3;      % Fuel-empty mass of rocket
-u_bar = 30 * 10^4;      % Maximum radial thrust
-k_m   =      10^2;      % Fuel consumption coefficient
+m0    = 20 * 10^3;              % Init. rocket mass (kg)
+m_e   =  1 * 10^3;              % Empty rocket mass (kg)
+% u_bar = 30 * 10^4;              % Max. thrust (N)
+u_bar = 30 * 10^1;              % Max. thrust (kN)
+k_m   =      10^2;              % Fuel consumption coeff. (kg * s^-1)
 
 %% ----------------- System ODE -------------------------------------------
 % Coordinate frame origin is center of moon (Polar coordinates)
@@ -20,10 +23,21 @@ k_m   =      10^2;      % Fuel consumption coefficient
 % x(3) -- Velocity
 % x(4) -- Angular velocity
 % x(5) -- Mass
+% xdot = @(x,u) [
+%     x(3); ...
+%     x(4); ...
+%     u(1)*u_bar/x(5)  - mu/x(1)^2 + x(1)*x(4)^2; ...
+%     u(2)*u_bar/(x(5)*x(1)) - 2*x(3)*x(4)/x(1) ; ...
+%     - k_m * (u(1)^2 + u(2)^2)
+%     ];
+
+% Scaled system
+% Distance in kilometers (altitude)
+% Angular velocity in microrads
 xdot = @(x,u) [
     x(3); ...
     x(4); ...
-    u(1)*u_bar/x(5)  - mu/x(1)^2 + x(1)*x(4)^2; ...
-    u(2)*u_bar/(x(5)*x(1)) - 2*x(3)*x(4)/x(1) ; ...
+    u(1)*u_bar/x(5)  - mu/(x(1)+R)^2 + 10^-12 * (x(1)+R)*x(4)^2; ...
+    10^6 * u(2)*u_bar/(x(5)*(x(1)+R)) - 2*x(3)*x(4)/(x(1)+R) ; ...
     - k_m * (u(1)^2 + u(2)^2)
     ];
