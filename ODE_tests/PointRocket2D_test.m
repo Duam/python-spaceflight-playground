5d3679a15937f1029d0173ea7a087773535e645c
Paul <paul@kiteswarms.com>
2017-10-05 17:18:35 +0200

    Fixed scaling fix, changed controls back to original ones. Works now.

--- ODE_tests/PointRocket2D_test.m
+++ ODE_tests/PointRocket2D_test.m
@@ -6,12 +6,10 @@ import casadi.*
 run ODEs/PointRocket2D.m
 
 %% Parameters (All in kg, m, s)
-% Target orbit
-T_orbit = 2*pi*sqrt(R^3 / mu) * 2;
-% R_T = R + 20000;
-% R_T = R + 20;
-% h_T = 20000;
+% Altitude of target orbit
 h_T = 20;
+% Orbital angular velocity in microradians
+angVel_T = 10^6 * sqrt(mu/(R+10^3*h_T)^3); 
 
 % Simulation parameters
 T = 600;
@@ -19,9 +17,7 @@ N = 100;  % Number of samples
 DT = T/N; % Step size
 
 % Initial state
-% x0 = [R; pi/2; 0; 0; m0];
 x0 = [0; 0; 0; 0; m0];
-% x0 = [0; 0; 0; 0; m0];
 
 %% System model
 nx = 5;
@@ -42,20 +38,14 @@ end
 ode_d = Function('ode_d', {x,u}, {Xk}, {'x','u'}, {'xk'});
 
 %% Simulate system
-% Old controls, where scaling was wrong
-% n_r_stop = 60 ;
-% U_rad = [0.1075 *ones(1,n_r_stop), zeros(1,N-n_r_stop)];
-% n_theta_stop = 85;
-% U_tan = [0.2 * ones(1, n_theta_stop), zeros(1,N-n_theta_stop)];
-% U_tan = zeros(1,N);
-
-% New controls with correct scaling
+% Controls
 n_r_stop = 60 ;
-U_rad = [0.01 *ones(1,n_r_stop), zeros(1,N-n_r_stop)];
+U_rad = [0.1075 *ones(1,n_r_stop), zeros(1,N-n_r_stop)];
 n_theta_stop = 85;
-U_tan = [0.007 * ones(1, n_theta_stop), zeros(1,N-n_theta_stop)];
-
+U_tan = [0.2 * ones(1, n_theta_stop), zeros(1,N-n_theta_stop)];
 U = [U_rad; U_tan];
+
+% Simulate
 X(:,1) = x0;
 for i=2:N
     X(:,i) = full(ode_d(X(:,i-1), U(:,i-1)));
@@ -63,22 +53,15 @@ end
 
 %% -- Plot --
 tAxis = 0:DT:T-DT;
-% Altitude
-% r_0 =   R * ones(1,length(tAxis));
-% r_T = R_T * ones(1,length(tAxis));
-r_0 =   0 * ones(1,length(tAxis));
-r_T = h_T * ones(1,length(tAxis));
+r_0        = x0(1) * ones(1,length(tAxis));
+theta_0    = x0(2) * ones(1,length(tAxis));
+rDot_0     = x0(3) * ones(1,length(tAxis));
+thetaDot_0 = x0(4) * ones(1,length(tAxis));
+m_0        = x0(5) * ones(1,length(tAxis));
 
-% Angle
-theta_0 = zeros(1,length(tAxis));
-% Angular velocity limits
-thetaDot_0 = zeros(1,length(tAxis));
-% thetaDot_T = sqrt(mu/(R_T)^3) * ones(1,length(tAxis));
-% thetaDot_T = sqrt(mu/(R+h_T)^3) * ones(1,length(tAxis));
-thetaDot_T = 10^6 * sqrt(mu/(R+h_T)^3) * ones(1,length(tAxis));
-% Mass limits
-m_0 = m0 * ones(1,length(tAxis));
-m_T = m_e * ones(1,length(tAxis));
+r_T        = h_T * ones(1,length(tAxis));
+thetaDot_T = angVel_T * ones(1,length(tAxis));
+m_T        = m_e * ones(1,length(tAxis));
 
 figure(1);
 clf
@@ -99,7 +82,7 @@ subplot(2,1,2);
 % Plot radial velocity
 subplot(3,2,3);
 hold on
-plot(tAxis, zeros(1,N), '--r');
+plot(tAxis, rDot_0, '--r');
 plot(tAxis, X(3,:));
 ylabel('$\dot{r} [\frac{km}{s}]$', 'interpreter', 'latex');
 % Plot angular velocity
