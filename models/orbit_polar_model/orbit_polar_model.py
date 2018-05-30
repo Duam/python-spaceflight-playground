fdb02590c6fb7cd86045b2d307bfc70829216d66
Paul Daum <paul@kiteswarms.com>
2018-05-30 21:27:56 +0200

    Added saving of trajectory

--- models/orbit_polar_model/orbit_polar_model.py
+++ models/orbit_polar_model/orbit_polar_model.py
@@ -158,6 +158,8 @@ if __name__ == '__main__':
     # Import plotting library and runge kutta 4 integrator    
     import matplotlib.pyplot as plt
     from integrators.rk4step import rk4step_ode
+    from utils.conversion import *
+    from utils.xml_writer import * 
 
     # Create a spacecraft instance
     spacecraft = orbit_polar_model()
@@ -192,44 +194,39 @@ if __name__ == '__main__':
     # Create system model with CasADi
     x = cas.MX.sym('x', spacecraft.nx, 1)
     u = cas.MX.sym('u', spacecraft.nu, 1)
-    ode = cas.Function('ode', [x,u], [spacecraft.dynamics(x,u)])
-    ode_scl = cas.Function('ode_scl', [x,u], [spacecraft.dynamics_scaled(x,u)])
+    f = cas.Function('f', [x,u], [spacecraft.dynamics_scaled(x,u)])
 
     # Discretize spacecraft dynamics using rk4
     Xk = x
     for k in range(nn):
-        #Xk = rk4step_ode(ode, Xk, u, h)
-        Xk = rk4step_ode(ode_scl, Xk, u, h)
+        Xk = rk4step_ode(f, Xk, u, h)
 
-    ode_d = cas.Function('ode_d', [x,u], [Xk], ['x','u'], ['xk'])
-    ode_scl_d = cas.Function('ode_scl_d', [x,u], [Xk], ['x','u'], ['xk'])
+    F = cas.Function('F', [x,u], [Xk], ['x','u'], ['xk'])
 
     # Choose controls for simulation
-    us = np.zeros((N,spacecraft.nu))
+    us = np.zeros((spacecraft.nu,N))
     n_r_stop = 60
     n_theta_stop = 85
     
     us_r = np.zeros(N)
     us_r[0:n_r_stop] = 0.1075 * np.ones(n_r_stop)
-    us[:,0] = us_r
+    us[0,:] = us_r
     
     us_theta = np.zeros(N)
     us_theta[0:n_theta_stop] = 0.2 * np.ones(n_theta_stop)
-    us[:,1] = us_theta
+    us[1,:] = us_theta
     
     # Simulate the system
-    xs = cas.DM.zeros((N, spacecraft.nx))
-    #xs[0,:] = spacecraft.x0
-    xs[0,:] = spacecraft.x0_scaled
+    xs = cas.DM.zeros((spacecraft.nx,N+1))
+    xs[:,0] = spacecraft.x0_scaled
 
-    for k in range(1,N):
-        #xs[k,:] = ode_d(xs[k-1,:],us[k-1,:])
-        xs[k,:] = ode_scl_d(xs[k-1,:],us[k-1,:])
+    for k in range(1,N+1):
+        xs[:,k] = F(xs[:,k-1],us[:,k-1])
 
     xs = xs.full()
 
     # Prepare plotting
-    tAxis = np.linspace(0, T-DT, N)
+    tAxis = np.linspace(0, T-DT, N+1)
     plt.figure(1)
 
     # Reference altitude [km] & angular velocity [µrad]
@@ -241,27 +238,46 @@ if __name__ == '__main__':
 
     # Plot 
     plt.subplot(321)
-    plt.plot(tAxis, xs[:,0])
+    plt.plot(tAxis, xs[0,:])
     plt.plot(tAxis, altitude_ref * np.ones((tAxis.size,1)))
     plt.ylabel('Altitude [km]')
 
     plt.subplot(322)
-    plt.plot(tAxis, xs[:,1])
+    plt.plot(tAxis, xs[1,:])
     plt.plot(tAxis, spacecraft.x0[1].full() * np.ones((tAxis.size,1)))
     plt.ylabel('Angle [murad]')
 
     plt.subplot(323)
-    plt.plot(tAxis, xs[:,2])
+    plt.plot(tAxis, xs[2,:])
     plt.plot(tAxis, spacecraft.x0[2].full() * np.ones((tAxis.size,1)))
     plt.ylabel('Radial vel. [km/s]')
 
     plt.subplot(324)
-    plt.plot(tAxis, xs[:,3])
+    plt.plot(tAxis, xs[3,:])
     plt.plot(tAxis, angVel_ref * np.ones((tAxis.size,1)))
     plt.ylabel('Angular vel. [murad/s]')
 
     plt.subplot(325)
-    plt.plot(tAxis, xs[:,4])
+    plt.plot(tAxis, xs[4,:])
     plt.ylabel('Mass [kg]')
 
     plt.show()
+
+    # Convert trajectory to cartesian coordinates
+    xs_cart, us_cart = traj_pol2cart(xs[0:4,:], us)
+
+    # Write trajectory to xml file
+    write_to_xml(
+        filename = 'trajectory_cartesian.xml',
+        T = T,
+        N = N,
+        e = [0,0,0],
+        h = [0,0,2],
+        xPoses = xs_cart[0,:],
+        yPoses = xs_cart[1,:],
+        xVelos = xs_cart[2,:],
+        yVelos = xs_cart[3,:],
+        masses = xs[4,:],
+        xForces = us_cart[0,:],
+        yForces = us_cart[1,:]
+    )
