51f06d1d4581cce591d6fed91ae316d051d9849b
Paul Daum <paul@kiteswarms.com>
2018-06-03 02:58:37 +0200

    Moved away test code. Fixed member variable access bug

--- models/liftoff_model/liftoff_model.py
+++ models/liftoff_model/liftoff_model.py
@@ -29,22 +29,22 @@ class liftoff_model:
         # Universe parameters
 
         # Gravitational constant (m/s^2)
-        self.g = params['g']
+        self.g = self.params['g']
 
         # Spacecraft parameters
 
         # Distance from COM to base (m)
-        self.L = params['L']
+        self.L = self.params['L']
         # Distance from COM to COP (m)
-        self.l = params['l']
+        self.l = self.params['l']
         # Maximum thrust (N)
-        self.maxThrust = params['maxThrust']
+        self.maxThrust = self.params['maxThrust']
         # Maximum gimbal angle in both directions (rad)
-        self.maxGimbal = params['maxGimbal']
+        self.maxGimbal = self.params['maxGimbal']
         # Total mass (kg)
-        self.m = params['m']
+        self.m = self.params['m']
         # Inertia tensor (kg*m^2)
-        self.I = params['I']
+        self.I = self.params['I']
 
         # Dynamics parameters
 
@@ -148,105 +148,3 @@ class liftoff_model:
 
         return xdot
 
-
-##
-# Run this script to test the model and generate a trajectory
-##
-if __name__ == '__main__':
-
-    import sys, os
-    sys.path.append(os.path.realpath('../../'))
-    sys.path.append(os.getcwd())
-
-    # Import plotting library and runge kutta 4 integrator    
-    import matplotlib.pyplot as plt
-    from integrators.rk4step import rk4step_ode
-
-    # Create a spacecraft instance
-    spacecraft = liftoff_model()
-
-    # Print some parameters
-    print("Spacecraft parameters:")
-    print("Grav. accel. g: " + str(spacecraft.g) + " m/s^2")
-    print("Distance base to COM: " + str(spacecraft.L) + " m")
-    print("Distance base to COP: " + str(spacecraft.l) + " m")
-    print("Mass: " + str(spacecraft.m) + " kg")
-    print("Max. thrust: " + str(spacecraft.maxThrust) + " N")
-    print("Initial state: " + str(spacecraft.x0))
-    print("Number of states: " + str(spacecraft.nx))
-    print("Number of disturbances: " + str(spacecraft.nd))
-    print("Number of controls: " + str(spacecraft.nu))
-
-    # Simulation parameters
-    T = 600.0
-    N = 100
-    DT = T/N
-
-    # Integration parameters 
-    nn = 10     # Integrator steps per step
-    h = DT/nn   # Step size of integrator step
-
-    # Create system model with CasADi
-    x = cas.MX.sym('x', spacecraft.nx, 1)
-    u = cas.MX.sym('u', spacecraft.nu + spacecraft.nd, 1)
-    f = cas.Function('f', [x,u], [spacecraft.dynamics(x,u)])
-    
-    # Discretize spacecraft dynamics using rk4
-    Xk = x
-    for k in range(nn):
-        Xk = rk4step_ode(f, Xk, u, h)
-    
-    F = cas.Function('F', [x,u], [Xk], ['x','u'], ['xk'])
-
-    # Choose controls for simulation
-    wind_forces = np.zeros((N, spacecraft.nd))
-    controls = np.zeros((N, spacecraft.nu))
-    controls[:,0] = 0.7 * np.ones(N)
-    controls[50:51,1] = 0.000001 * np.ones(1)
-
-    us = np.append(controls, wind_forces, axis=1)
-
-    # Test evaluation of the ODE
-    print(us[0,:])
-    xdot_test = spacecraft.dynamics(spacecraft.x0, us[0,:])
-    print(xdot_test)
-
-    # Simulate the system
-    xs = cas.DM.zeros((N, spacecraft.nx))
-    xs[0,:] = spacecraft.x0
-
-    for k in range(1,N):
-        xs[k,:] = F(xs[k-1,:], us[k-1,:])
-
-    xs = xs.full()
-
-    # Prepare plotting
-    tAxis = np.linspace(0, T-DT, N)
-    plt.figure(1)
-
-    # Plot
-    plt.subplot(321)
-    plt.plot(tAxis, xs[:,0])
-    plt.ylabel('x-pos [m]')
-
-    plt.subplot(322)
-    plt.plot(tAxis, xs[:,1])
-    plt.ylabel('y-pos [m]')
-
-    plt.subplot(323)
-    plt.plot(tAxis, xs[:,2])
-    plt.ylabel('x-vel [m/s]')
-
-    plt.subplot(324)
-    plt.plot(tAxis, xs[:,3])
-    plt.ylabel('y-vel [m/s]')
-
-    plt.subplot(325)
-    plt.plot(tAxis, xs[:,4])
-    plt.ylabel('angle [rad]')
-
-    plt.subplot(326)
-    plt.plot(tAxis, xs[:,5])
-    plt.ylabel('angular vel. [rad/s]')
-
-    plt.show()
