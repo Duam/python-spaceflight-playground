057272fc3c886129f4d32d4d70e9df6bde31346f
Paul Daum <paul.daum@posteo.de>
2021-08-18 19:12:22 +0200

    Refactored kepler orbit.

--- src/spaceflight_playground/models/kepler_orbit.py
+++ src/spaceflight_playground/models/kepler_orbit.py
@@ -1,253 +1,144 @@
-#!/usr/bin/python3
-
-##
-# @file kepler_orbit.py
-# @author Paul Daum
-##
-
 import numpy as np
+from typing import Tuple
+from spaceflight_playground.conversion import Universe
+from spaceflight_playground.conversion import pol2cart, state_pol2cart
 
-import sys, os
-sys.path.append(os.path.realpath('../../../'))
-sys.path.append(os.getcwd())
 
-from src.spaceflight_playground.conversion import pol2cart, state_pol2cart
-
-"""
-# Model
-TODO
-
-## Parameters
-TODO
-
-## Methods
-TODO
-
-## Coordinate system definitions
-- X axis is horizontal, points right (vernal equinox)
-- Y axis is vertical, points upwards
-- Z axis points out of the screen (Body's rotational axis)
-"""
-
-##
-# @class kepler_orbit
-# @brief Representation of a keplerian orbit. Convenience class
-#        for easy computation of kepler elements from cartesian state vector.
-#        Should later be compatible with polar state vector.
-##
-class kepler_orbit:
-
-    ##
-    # @brief Initialization procedure
-    ##
+class KeplerOrbit:
     def __init__(self):
+        """Representation of a keplerian orbit. Convenience class
+        for easy computation of kepler elements from cartesian state vector.
+
+        Coordinate system definitions:
+        - X axis is horizontal, points right (vernal equinox)
+        - Y axis is vertical, points upwards
+        - Z axis points out of the screen (Body's rotational axis)
+        """
         # Universe parameters
-        
-        # Gravitational constant (m^3/(kg*s^2))
-        self.G = 6.67408 * 10**-11
-        # Moon mass (kg) TODO should be settable
-        self.M = 7.348 * 10**22
         # Std. gravitational parameter (m^3/s^2)
-        self.mu = self.G * self.M
-        # Moon radius (m)
-        self.R = 1737.5 * 10**3
-        
+        self.mu = Universe.gravitational_constant * Universe.moon_mass
+
         # Orbital elements
-
         # Eccentricity vector (in 2d only the first two elements are non-zero)
-        self.e = np.array([0,0,0])
+        self.eccentricity = np.array([0, 0, 0])
         # Specific orbital angular momentum (in 2d only the last element is non-zero)
-        self.h = np.array([0,0,0])
-        
-    ##
-    # @brief Returns a string that contains the orbital elements
-    ##
-    def toString(self):
-        return "e=" + str(self.e) + ", h=" + str(self.h)
+        self.specific_angular_momentum = np.array([0, 0, 0])
+
+    def __str__(self):
+        return f"eccentricity = {self.eccentricity}, angular momentum = {self.specific_angular_momentum}"
+
+    def setOrbitalElements(self, eccentricity: np.ndarray, specific_angular_momentum: float) -> None:
+        """Setter for the orbital elements
+        :param eccentricity: The orbit's eccentricity.
+        :param specific_angular_momentum: The orbit's specific orbital angular momentum.
+        :return: None
+        """
+        self.eccentricity = eccentricity
+        self.specific_angular_momentum = specific_angular_momentum
 
 
-    ##
-    # @brief Setter for the orbital elements
-    # @param e Eccentricity vector (3x1)
-    # @param h Specific orbital angular momentum vector (3x1)
-    # @return Nothing
-    ##
-    def setOrbitalElements(self, e, h):
-        self.e = e
-        self.h = h
+    def fromEllipseParams(
+            self,
+            eccentricity: float,
+            angle: float,
+            big_semi_major_axis: float
+    ) -> Tuple[float, float]:
+        """Computes kepler elements from common ellipse parameters.
+        :param eccentricity: Eccentricity (Scalar value)
+        :param angle: Angle of the orbit
+        :param big_semi_major_axis: Big semi-major axis
+        :return: Eccentricity vector, specific orbital angular momentum
+        """
+        # Compute the norm of specific orbital angular momentum
+        semi_latus_rectum = big_semi_major_axis * (1 - eccentricity ** 2)
+        specific_angular_momentum = np.sqrt(semi_latus_rectum * self.mu)
 
-    ##
-    # @brief Computes kepler elements from common ellipse parameters.
-    # @param e Eccentricity (Scalar value)
-    # @param angle Angle of the orbit
-    # @param a Big semi-major axis
-    # @return e Eccentricty vector
-    # @return h Specific orbital angular momentum
-    ## 
-    def fromEllipseParams(self, e, angle, a):
-        
-        # Compute semi-latus rectup
-        p = a * (1 - e**2)
-
-        # Compute norm of specific orbital angular momentum
-        h = np.sqrt(p * self.mu)
-
-        # Since we are in 2d, that's enough for to momentum vector
-        self.h = np.array([0, 0, h])
-
-        # Compute elements of the eccentricity vector
-        e_x = e * np.cos(angle)
-        e_y = e * np.sin(angle)
-
-        # Since we are in 2d, that's enought for the eccentricity vector
-        self.e = np.array([e_x, e_y, 0])
-
-        # return
-        return self.e, self.h
+        # Compute and update kepler elements in 2d, lifted into 3d
+        self.specific_angular_momentum = np.array([0, 0, specific_angular_momentum])
+        self.eccentricity = np.array([eccentricity * np.cos(angle), eccentricity * np.sin(angle), 0])
+        return self.eccentricity, self.specific_angular_momentum
 
 
-    ##
-    # @brief Computes kepler elements from cartesian state vector.
-    #        Updates the internal orbit parameters.
-    # @param xPos x-position
-    # @param yPos y-position
-    # @param xVel x-velocity
-    # @param yVel y-velocity
-    # @return e eccentricity vector
-    # @return h specific orbital angular momentum
-    ##
-    def fromCartesianState (self, xPos, yPos, xVel, yVel):
+    def fromCartesianState (
+            self,
+            position_x: float,
+            position_y: float,
+            velocity_x: float,
+            velocity_y: float
+    ) -> Tuple[float, float]:
+        """Computes kepler elements from a cartesian state vector.
+        Updates the internal orbit parameters.
+        :param position_x: The x-position of the orbitting object.
+        :param position_y: The y-position of the orbitting object.
+        :param velocity_x: The x-velocity of the orbitting object.
+        :param velocity_y: The y-velocity of the orbitting object.
+        :return: Eccentricity vector, specific orbital angular momentum.
+        """
+        # Expand dimensionality and norm the position
+        position = np.array([position_x, position_y, 0])
+        velocity = np.array([velocity_x, velocity_y, 0])
+        normed_position = position / np.linalg.norm(position)
 
-        # Expand dimensionality of position and velocity (required for cross product)
-        pos = np.array([xPos, yPos, 0])
-        vel = np.array([xVel, yVel, 0])
-
-        # Precompute the distance from the origin
-        pos_norm = np.linalg.norm(pos)
-
-        # Compute and update the orbital elements
-        self.h = np.cross(pos, vel)
-        self.e = np.cross(vel, self.h) / self.mu - pos / pos_norm
-        return self.e, self.h
-
-
-    ##
-    # @brief Computes kepler elementes from polar state vector.
-    #        Updates the internal orbit parameters. TODO COULD BE OPTIMIZED.
-    # @param rho
-    # @param theta
-    # @param rhoDot
-    # @param thetaDot
-    # @return e eccentricity vector
-    # @return h specific orbital angular momentum
-    ##
-    def fromPolarState (self, rho, theta, rhoDot, thetaDot):
-
-        # Convert polar coordinates to cartesian coordinates
-        x_pol = np.array([rho, theta, rhoDot, thetaDot])
-        x_cart = state_pol2cart(x_pol)
-        
-        # Get the vector elements
-        xPos = x_cart[0]
-        yPos = x_cart[1]
-        xVel = x_cart[2]
-        yVel = x_cart[3]
-
-        # Compute orbital elements and return
-        self.fromCartesianState(xPos, yPos, xVel, yVel)
-        return self.e, self.h
+        # Compute and update kepler elements
+        self.specific_angular_momentum = np.cross(position, velocity)
+        self.eccentricity = np.cross(velocity, self.specific_angular_momentum) / self.mu - normed_position
+        return self.eccentricity, self.specific_angular_momentum
 
 
-    ##
-    # @brief Discretizes the orbit into a bunch of positions.
-    #        Mainly used for plotting. Could also be used for
-    #        pathfollowing (?).
-    # @param N Number of samples
-    # @return An Nx2 vector representing the discretized orbit
-    ##
-    def discretize (self, N=360):
+    def fromPolarState (
+            self,
+            distance: float,
+            angle: float,
+            radial_velocity: float,
+            angular_velocity: float
+    ) -> Tuple[float, float]:
+        """Computes kepler elements from polar state vector.
+        Updates the internal orbit parameters.
+        :param distance: The orbiter's distance to the origin.
+        :param angle: The orbiter's angle, measured counter-clockwise from the positive x-axis.
+        :param radial_velocity: The orbiter's velocity away from the origin.
+        :param angular_velocity: The orbiter's angular velocity.
+        :return: Eccentricity vector, specific orbital angular momentum.
+        """
+        # Convert polar coordinates to cartesian coordinates and compute kepler elements
+        state_polar = np.array([distance, angle, radial_velocity, angular_velocity])
+        state_cartesian = state_pol2cart(state_polar)
+        return self.fromCartesianState(state_cartesian[0], state_cartesian[1], state_cartesian[2], state_cartesian[3])
 
-        # Precompute norms
-        e_norm = np.linalg.norm(self.e)
-        h_norm = np.linalg.norm(self.h)
 
-        # Compute semi-latus rectum
-        p = h_norm**2 / self.mu
+    def discretize (self, num_samples=360) -> np.ndarray:
+        """Discretizes the orbit into a bunch of positions.
+        :param num_samples: Number of discrete samples.
+        :return: A Nx2 vector representing the discretized orbit.
+        """
+        eccentricity_norm = np.linalg.norm(self.eccentricity)
+        angular_momentum_norm = np.linalg.norm(self.specific_angular_momentum)
+        semi_latus_rectum = angular_momentum_norm**2 / self.mu
 
         # Compute rotation of the ellipse
-        beta = np.arctan2(self.e[1], self.e[0])
+        ellise_angle = np.arctan2(self.eccentricity[1], self.eccentricity[0])
         
         # Prepare samples container
-        samples = np.zeros((N,2))
+        samples = np.zeros((num_samples, 2))
 
         # Discretize the ellipse
-        angles = np.linspace(0, 2*np.pi, N)
-        for k in range(N):
+        angles = np.linspace(0, 2 * np.pi, num_samples)
+        for k in range(num_samples):
             # Get current angle
-            theta = angles[k]
+            angle = angles[k]
             
             # Compute radius at current angle ("orbit equation")
-            rho = p / (1 + e_norm * np.cos(theta))
+            distance = semi_latus_rectum / (1 + eccentricity_norm * np.cos(angle))
 
             # Rotate the orbit clockwise
-            theta = theta + beta
+            angle = angle + ellise_angle
 
             # Convert into cartesian coordinates
-            pos = pol2cart(theta, rho)
+            position = pol2cart(angle, distance)
 
             # Store coordinates in container
-            samples[k,0] = pos[0]
-            samples[k,1] = pos[1]
+            samples[k, 0] = position[0]
+            samples[k, 1] = position[1]
 
         # Return the discretized orbit
         return samples
-
-
-## 
-# Run this script to test the orbit functions
-##
-if __name__ == '__main__':
-
-    # Import plotting library
-    import matplotlib.pyplot as plt
-
-    # Create an orbit instance
-    orbit = kepler_orbit()
-
-    # Print parameters
-    print("Orbit parameters:")
-    print("Eccentricity vector: " + str(orbit.e))
-    print("Angular momentum vector: " + str(orbit.h))
-
-    # Set the orbit using the orbital parameters directly
-    e = np.array([0.3, 0.3, 0])
-    h = np.array([0, 0, 3000000])
-
-    orbit.setOrbitalElements(e,h)
-    print(orbit.toString())
-    samples_0 = orbit.discretize()
-
-    # Set the orbit using polar state vector
-    orbit.fromPolarState(orbit.R + 20000.0, 0.0, 0.0, 0.00095046751314)
-    print(orbit.toString())
-    samples_1 = orbit.discretize()
-
-    # Set the orbit using ellipse parameters
-    orbit.fromEllipseParams(0.1, 0.0, 1.0)
-    print(orbit.toString())
-    samples_2 = orbit.discretize()
-    
-    orbit.fromCartesianState(0.0, orbit.R + 20000.0, 1500.0, 0.0)
-    print(orbit.toString())
-    samples_3 = orbit.discretize()
-
-    # Plot orbit
-    #plt.plot(samples_0[:,0], samples_0[:,1])
-    plt.plot(samples_1[:,0], samples_1[:,1])
-    #plt.plot(samples_2[:,0], samples_2[:,1])
-    plt.plot(samples_3[:,0], samples_3[:,1])
-
-    #plt.xlim([-2,2])
-    #plt.ylim([-2,2])
-    plt.grid(True)
-    plt.show()
---
+++ tests/test_kepler_orbit.py
@@ -0,0 +1,49 @@
+import numpy as np
+from spaceflight_playground.constants import Universe
+from spaceflight_playground.models.kepler_orbit import KeplerOrbit
+
+if __name__ == '__main__':
+    # Import plotting library
+    import matplotlib.pyplot as plt
+
+    # Create an orbit instance
+    orbit = KeplerOrbit()
+    R = Universe.moon_radius
+
+    # Print parameters
+    print("Orbit parameters:")
+    print("Eccentricity vector: " + str(orbit.eccentricity))
+    print("Angular momentum vector: " + str(orbit.specific_angular_momentum))
+
+    # Set the orbit using the orbital parameters directly
+    e = np.array([0.3, 0.3, 0])
+    h = np.array([0, 0, 3000000])
+
+    orbit.setOrbitalElements(e, h)
+    print(orbit)
+    samples_0 = orbit.discretize()
+
+    # Set the orbit using polar state vector
+    orbit.fromPolarState(R + 20000.0, 0.0, 0.0, 0.00095046751314)
+    print(orbit)
+    samples_1 = orbit.discretize()
+
+    # Set the orbit using ellipse parameters
+    orbit.fromEllipseParams(0.1, 0.0, 1.0)
+    print(orbit)
+    samples_2 = orbit.discretize()
+
+    orbit.fromCartesianState(0.0, R + 20000.0, 1500.0, 0.0)
+    print(orbit)
+    samples_3 = orbit.discretize()
+
+    # Plot orbit
+    # plt.plot(samples_0[:,0], samples_0[:,1])
+    plt.plot(samples_1[:, 0], samples_1[:, 1])
+    # plt.plot(samples_2[:,0], samples_2[:,1])
+    plt.plot(samples_3[:, 0], samples_3[:, 1])
+
+    # plt.xlim([-2,2])
+    # plt.ylim([-2,2])
+    plt.grid(True)
+    plt.show()
