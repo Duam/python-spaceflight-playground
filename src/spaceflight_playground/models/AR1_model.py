4fef5b61eeff88c421625e1c76a44c3611c7f6cb
Paul Daum <paul.daum@posteo.de>
2021-08-19 11:15:03 +0200

    Refactored AR1 model

--- src/spaceflight_playground/models/AR1_model.py
+++ src/spaceflight_playground/models/AR1_model.py
@@ -1,80 +1,42 @@
-#!/usr/bin/python3
-
 import numpy as np
 
-"""
-# Model
-This model is used for simulating windspeeds in one direction. The wind
-changes each second, therefore it's modelled using a gaussian distribution.
-The timeseries is generated using an AR(1) model.
-
-## Initialization parameters
-- phi: The AR(1) parameter (default = 0.3)
-- mean: Mean value of the timeseries (default = 0)
-- variance: Variance of the timeseries (default = 1)
-
-## Usage
-1. Initialize the model using the above parameters
-2. Use the update() function to compute the next sample. It also returns the new sample
-
-- Use getCurrentSample() to get the current sample without triggering the update.
-- Use updateParameters(phi, mean, variance) to update the parameters whenever they change
-"""
-
-##
-# @class AR1_model
-# @brief An order 1 autoregressive model with
-#        a gaussian seed. Used for simulating
-#        wind speeds in one direction.
-##
 class AR1_model:
-
-    ##
-    # @brief Initialization procedure
-    # @param phi Autoregression parameter
-    # @param mean Mean of the timeseries
-    # @param variance Variance of the timeseries (must be > 0)
-    ##
-    def __init__(self, phi=0.3, mean=0, variance=1):
-        # Process mean
-        self.mean = mean
-        # Process variance
-        self.variance = variance
-        # Autoregression parameter
-        self.phi = phi
-        # Constant term in the AR(1) process (derived parameter)
-        self.c = self.mean * (1 - self.phi)
-        # Standard deviation of the gaussian seed (derived parameter)
-        self.normdist_stddev = np.sqrt((1 - self.phi**2) * self.variance)
-        # Current sample initialization
+    def __init__(self, autoregressivity: float=0.3, mean: float=0, variance: float=1):
+        """An order 1 autoregressive model with a gaussian seed. Used for simulating
+        wind speeds in one direction.
+        :param autoregressivity: Autoregression parameter. Higher -> values change slower.
+        :param mean: The mean of the timeseries.
+        :param variance: The variance of timeseries.
+        """
+        self.set_parameters(autoregressivity, mean, variance)
         self.xCur = mean
-        
-    ##
-    # @brief Parameter update function
-    # @param phi Autoregression parameter
-    # @param mean Mean of the timeseries
-    # @param variance Variance of the timeseries (must be > 0)
-    ##
-    def updateParameters(self, phi=0.3, mean=0, variance=1):
+
+
+    def set_parameters(self, autoregressivity: float, mean: float, variance: float) -> None:
+        """Parameter update function.
+        :param autoregressivity: Autoregression parameter. Higher -> values change slower.
+        :param mean: Mean of the timeseries.
+        :param variance: Variance of the timeseries.
+        :return: None
+        """
+        assert variance > 0, f"Variance must be positive, ist {variance}"
         self.mean = mean
         self.variance = variance
-        self.phi = phi
-        self.c = self.mean * (1 - self.phi)
-        self.normdist_stddev = np.sqrt((1 - self.phi**2) * self.variance)
+        self.autoregressivity = autoregressivity
 
 
     def sample_normal(self):
         """Wrapper for numpy normal sampler
         :return: Normally distributed sample.
         """
-        return np.random.normal(0, self.normdist_stddev)
+        return np.random.normal(0, np.sqrt((1 - self.autoregressivity ** 2) * self.variance))
 
 
     def update(self):
         """Computes the next sample and updates the internal value.
         :return: The next sample.
         """
-        self.xCur = self.c + self.phi * self.xCur + self.sample_normal()
+        self.xCur = self.mean * (1 - self.autoregressivity) + self.autoregressivity * self.xCur + self.sample_normal()
         return self.xCur
 
 
--- tests/test_AR1_model.py
+++ tests/test_AR1_model.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 from spaceflight_playground.models.AR1_model import AR1_model
 
 if __name__ == '__main__':
-    ar1 = AR1_model(phi=0.3, mean=5, variance=10)
+    ar1 = AR1_model(autoregressivity=0.3, mean=5, variance=10)
     print("AR1-model parameters:")
     print("Process mean = " + str(ar1.mean))
     print("Process variance = " + str(ar1.variance))
-    print("Autoregression parameter phi = " + str(ar1.phi))
+    print("Autoregression parameter phi = " + str(ar1.autoregressivity))
     print("Autoregression constant parameter c = " + str(ar1.c))
     print("Normal distribution stddev = " + str(ar1.normdist_stddev))
     print("Initialized sample value = " + str(ar1.xCur))
