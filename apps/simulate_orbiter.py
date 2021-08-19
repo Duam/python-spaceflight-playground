a970d798baa33e4a84c5d639046766c4933c8caa
Paul Daum <paul.daum@posteo.de>
2021-08-19 14:46:06 +0200

    Made orbiter simulation run again

--- apps/simulate_orbiter.py
+++ apps/simulate_orbiter.py
@@ -3,132 +3,142 @@ import casadi as cas
 import matplotlib.pyplot as plt
 from spaceflight_playground.rk4step import rk4step_ode
 from spaceflight_playground.aux_models.kepler_orbit import KeplerOrbit
-from spaceflight_playground.orbiter.orbiter_model import PolarOrbiter
-from spaceflight_playground.conversion import *
-from spaceflight_playground.xml_writer import *
+from spaceflight_playground.orbiter.orbiter_model import (
+    PolarOrbiter,
+    PolarOrbiterState,
+    PolarOrbiterThrust
+)
 
-if __name__ == '__main__':
-    spacecraft = PolarOrbiter()
+spacecraft = PolarOrbiter()
 
-    # Simulation parameters
-    T = 600.0
-    N = 100
-    DT = T / N
+# Simulation parameters
+total_time = 600.0  # [s]
+num_samples = 100
+timestep = total_time / num_samples
 
-    # Integration parameters
-    nn = 10  # Integrator steps per step
-    h = DT / nn  # Step size of integrator step
+# Create discretized system model with CasADi
+x = cas.MX.sym('x', spacecraft.num_states, 1)
+u = cas.MX.sym('u', spacecraft.num_forces, 1)
+state_derivative_fun = cas.Function(
+    'ode', [x, u],
+    [spacecraft.ode_scaled(PolarOrbiterState(x), PolarOrbiterThrust(u)).vector],
+    ['state', 'thrust'], ['state_derivative'])
+next_state = rk4step_ode(state_derivative_fun, x, u, timestep)
+next_state_fun = cas.Function('next_state', [x, u], [next_state], ['x', 'u'], ['xnext'])
 
-    # Create system model with CasADi
-    x = cas.MX.sym('x', spacecraft.num_states, 1)
-    u = cas.MX.sym('u', spacecraft.num_forces, 1)
-    f = cas.Function('f', [x, u], [spacecraft.ode_scaled(x, u)])
+# Initial state
+initial_state = PolarOrbiterState(
+    cas.DM([
+        spacecraft.moon_radius,
+        0,
+        0,
+        0,
+        spacecraft.full_mass
+    ])
+).scale(spacecraft.scale)
+print(f"Initial state: {initial_state.vector}")
 
-    # Discretize spacecraft dynamics using rk4
-    Xk = x
-    for k in range(nn):
-        Xk = rk4step_ode(f, Xk, u, h)
+# Choose controls for simulation
+thrusts = np.zeros((spacecraft.num_forces, num_samples))
+thrust_radial_stop = 60
+thrust_angular_stop = 85
 
-    F = cas.Function('F', [x, u], [Xk], ['x', 'u'], ['xk'])
+radial_thrusts = np.zeros(num_samples)
+radial_thrusts[0:thrust_radial_stop] = 0.1075 * np.ones(thrust_radial_stop)
+thrusts[0, :] = radial_thrusts
 
-    # Choose controls for simulation
-    us = np.zeros((spacecraft.num_forces, N))
-    n_r_stop = 60
-    n_theta_stop = 85
+angular_thrusts = np.zeros(num_samples)
+angular_thrusts[0:thrust_angular_stop] = 0.2 * np.ones(thrust_angular_stop)
+thrusts[1, :] = angular_thrusts
 
-    us_r = np.zeros(N)
-    us_r[0:n_r_stop] = 0.1075 * np.ones(n_r_stop)
-    us[0, :] = us_r
+# Simulate the system
+xs = cas.DM.zeros((spacecraft.num_states, num_samples + 1))
+xs[:, 0] = initial_state.vector
 
-    us_theta = np.zeros(N)
-    us_theta[0:n_theta_stop] = 0.2 * np.ones(n_theta_stop)
-    us[1, :] = us_theta
+for k in range(1, num_samples + 1):
+    xs[:, k] = next_state_fun(xs[:, k - 1], thrusts[:, k - 1])
 
-    # Simulate the system
-    xs = cas.DM.zeros((spacecraft.num_states, N + 1))
-    xs[:, 0] = spacecraft.x0_scaled
+# Unscale trajectory
+for k in range(num_samples + 1):
+    xs[:, k] = xs[:, k] * spacecraft.unscale
 
-    for k in range(1, N + 1):
-        xs[:, k] = F(xs[:, k - 1], us[:, k - 1])
+xs = xs.full()
 
-    # Unscale trajectory
-    for k in range(N + 1):
-        xs[:, k] = xs[:, k] * spacecraft.unscale
+# Prepare plotting
+tAxis = np.linspace(0, total_time - timestep, num_samples + 1)
+plt.figure(1)
 
-    xs = xs.full()
+# Reference altitude [km] & angular velocity [µrad]
+altitude_ref = 20
+angVel_ref = 1e3 * np.sqrt(spacecraft.gravitational_constant * spacecraft.moon_mass /
+                           (spacecraft.moon_radius + 1e3 * altitude_ref) ** 3)
 
-    # Prepare plotting
-    tAxis = np.linspace(0, T - DT, N + 1)
-    plt.figure(1)
+print("Reference altitude: " + str(altitude_ref))
+print("Reference angular velocity: " + str(angVel_ref))
 
-    # Reference altitude [km] & angular velocity [µrad]
-    altitude_ref = 20
-    angVel_ref = 1e3 * np.sqrt(spacecraft.mu / (spacecraft.moon_radius + 1e3 * altitude_ref) ** 3)
+# Plot
+plt.subplot(321)
+plt.plot(tAxis, xs[0, :])
+plt.plot(tAxis, altitude_ref * np.ones((tAxis.size, 1)))
+plt.ylabel('Altitude [km]')
 
-    print("Reference altitude: " + str(altitude_ref))
-    print("Reference angular velocity: " + str(angVel_ref))
+plt.subplot(322)
+plt.plot(tAxis, xs[1, :])
+plt.plot(tAxis, initial_state.vector[1].full() * np.ones((tAxis.size, 1)))
+plt.ylabel('Angle [murad]')
 
-    # Plot
-    plt.subplot(321)
-    plt.plot(tAxis, xs[0, :])
-    plt.plot(tAxis, altitude_ref * np.ones((tAxis.size, 1)))
-    plt.ylabel('Altitude [km]')
+plt.subplot(323)
+plt.plot(tAxis, xs[2, :])
+plt.plot(tAxis, initial_state.vector[2].full() * np.ones((tAxis.size, 1)))
+plt.ylabel('Radial vel. [km/s]')
 
-    plt.subplot(322)
-    plt.plot(tAxis, xs[1, :])
-    plt.plot(tAxis, spacecraft.x0[1].full() * np.ones((tAxis.size, 1)))
-    plt.ylabel('Angle [murad]')
+plt.subplot(324)
+plt.plot(tAxis, xs[3, :])
+plt.plot(tAxis, angVel_ref * np.ones((tAxis.size, 1)))
+plt.ylabel('Angular vel. [murad/s]')
 
-    plt.subplot(323)
-    plt.plot(tAxis, xs[2, :])
-    plt.plot(tAxis, spacecraft.x0[2].full() * np.ones((tAxis.size, 1)))
-    plt.ylabel('Radial vel. [km/s]')
+plt.subplot(325)
+plt.plot(tAxis, xs[4, :])
+plt.ylabel('Mass [kg]')
 
-    plt.subplot(324)
-    plt.plot(tAxis, xs[3, :])
-    plt.plot(tAxis, angVel_ref * np.ones((tAxis.size, 1)))
-    plt.ylabel('Angular vel. [murad/s]')
+plt.show()
 
-    plt.subplot(325)
-    plt.plot(tAxis, xs[4, :])
-    plt.ylabel('Mass [kg]')
+quit(0)
 
-    plt.show()
+# Convert trajectory to cartesian coordinates
+for k in range(num_samples + 1):
+    xs[0, k] = xs[0, k] + spacecraft.moon_radius
 
-    # Convert trajectory to cartesian coordinates
-    for k in range(N + 1):
-        xs[0, k] = xs[0, k] + spacecraft.moon_radius
+# Unscale trajectory
+# for k in range(N+1):
+#    xs[:,k] = xs[:,k] * spacecraft.unscale
 
-    # Unscale trajectory
-    # for k in range(N+1):
-    #    xs[:,k] = xs[:,k] * spacecraft.unscale
+xs_cart, us_cart = traj_pol2cart(xs[0:4, :], thrusts)
 
-    xs_cart, us_cart = traj_pol2cart(xs[0:4, :], us)
+print(xs_cart[0, :])
+print(xs_cart[1, :])
 
-    print(xs_cart[0, :])
-    print(xs_cart[1, :])
+# Create an orbit instance
+orb = KeplerOrbit()
+orb.fromPolarState(
+    distance=spacecraft.moon_radius + spacecraft.unscale[0] * altitude_ref,
+    angle=0.0,
+    radial_velocity=0.0,
+    angular_velocity=spacecraft.unscale[3] * angVel_ref
+)
 
-    # Create an orbit instance
-    orb = KeplerOrbit()
-    orb.fromPolarState(
-        distance=spacecraft.moon_radius + spacecraft.unscale[0] * altitude_ref,
-        angle=0.0,
-        radial_velocity=0.0,
-        angular_velocity=spacecraft.unscale[3] * angVel_ref
-    )
-
-    # Write trajectory to xml file
-    write_to_xml(
-        filename='orbit_animator_trajectory.xml',
-        T=T,
-        N=N,
-        e=orb.eccentricity[0:2],
-        h=orb.specific_angular_momentum[2],
-        xPoses=xs_cart[0, :],
-        yPoses=xs_cart[1, :],
-        xVelos=xs_cart[2, :],
-        yVelos=xs_cart[3, :],
-        masses=xs[4, :],
-        xForces=us_cart[0, :],
-        yForces=us_cart[1, :]
-    )
+# Write trajectory to xml file
+write_to_xml(
+    filename='orbit_animator_trajectory.xml',
+    T=total_time,
+    N=num_samples,
+    e=orb.eccentricity[0:2],
+    h=orb.specific_angular_momentum[2],
+    xPoses=xs_cart[0, :],
+    yPoses=xs_cart[1, :],
+    xVelos=xs_cart[2, :],
+    yVelos=xs_cart[3, :],
+    masses=xs[4, :],
+    xForces=us_cart[0, :],
+    yForces=us_cart[1, :]
+)
